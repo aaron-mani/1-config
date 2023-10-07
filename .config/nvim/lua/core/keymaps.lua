eb9721e6f7c49593a28fe11c753f503c363490e9
Aaron Mani <Aaron.Mani@rubrik.com>
2023-10-06 19:03:22 -0700

    Cleanup keymaps.lua

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -9,22 +9,3 @@ keymap.set("n", "<leader>|", "<C-w>v") -- vertical split
 keymap.set("n", "<leader>-", "<C-w>s") -- horizontal split
 keymap.set("n", "<leader>=", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>x", ":close<CR>") -- split window horizontally 
-
--- recommended mappings
--- resizing splits
--- these keymaps will also accept a range,
--- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
--- vim.keymap.set('n', '<M-h>', require('smart-splits').resize_left)
--- vim.keymap.set('n', '<M-j>', require('smart-splits').resize_down)
--- vim.keymap.set('n', '<M-k>', require('smart-splits').resize_up)
--- vim.keymap.set('n', '<M-l>', require('smart-splits').resize_right)
--- -- moving between splits
--- vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
--- vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
--- vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
--- vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
--- -- swapping buffers between windows
--- vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
--- vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
--- vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
--- vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
