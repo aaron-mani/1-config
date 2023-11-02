d0ddcc315a1dcec3ebaada61cf46de02842507ab
Aaron Mani <Aaron.Mani@rubrik.com>
2023-11-01 19:37:46 -0700

    Add metals plugin and some keymaps

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,10 +1,38 @@
 return {
   "neovim/nvim-lspconfig",
-  -- Setup language servers.
   config = function()
     local lspconfig = require('lspconfig')
+    -- local lsp_defaults = lspconfig.util.default_config
+    -- Merge lspconfig default capabilties with nvim-cmp capabilities
+    -- lsp_defaults.capabilities = vim.tbl_deep_extend(
+    --   "force",
+    --   lsp_defaults.capabilities,
+    --   require("cmp_nvim_lsp").default_capabilities()
+    -- )
+
+    -- Setup language servers.
     lspconfig.pyright.setup {}
     lspconfig.tsserver.setup {}
-    lspconfig.lua_ls.setup {}
+    lspconfig.metals.setup {}
+    lspconfig.lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" }, -- remove undefined refs to vim
+          },
+        },
+      },
+    }
+    lspconfig.gopls.setup {
+      on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+        vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
+        vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
+      end,
+    }
   end,
 }
