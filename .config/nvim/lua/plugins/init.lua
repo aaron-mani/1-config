55f26ac9db962cc96865aaf52670cf6d29f64e19
Aaron Mani <Aaron.Mani@rubrik.com>
2024-09-17 12:15:16 -0700

    Add minor comment

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "tpope/vim-fugitive",
+  "tpope/vim-fugitive",    -- vim plugin for git
   "jiangmiao/auto-pairs",
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 }
