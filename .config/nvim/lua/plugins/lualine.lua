13a0cd7f862242f2e5ea7bf601733be13e7608dd
Aaron Mani <aaron.mani@rubrik.com>
2026-03-19 21:21:40 +0530

    Minor edits 2

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,7 +8,6 @@ return {
     -- configure lualine
     lualine.setup({
       options = {
-        -- theme = 'onedark',
         icons_enabled = true,
         component_separators = '|',
         section_separators = '',
